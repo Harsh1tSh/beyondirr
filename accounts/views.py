4182776b2af3dc0678b0530938bad44079cd207e
Harshit Sharma <hepta1000@gmail.com>
2024-08-18 23:01:21 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -25,7 +25,6 @@ class UserSignupView(views.APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# Should i use decorator here too?  
 class TransactionUploadView(views.APIView):
     parser_classes = (MultiPartParser, FormParser)
     @log_request(record_success=True)
